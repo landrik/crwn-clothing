caf8854eae4ec57c84556fce3b46e620285768c6
Landry Karege <landrik@gmail.com>
2020-05-18 13:49:00 +0100

    moved our directory and shop into own reducer, created recpective selectores and updated corresponding components with new redux flow for directory/shop

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,16 +2,16 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-    apiKey: "AIzaSyArtvNfiP-5OoJnywHt0304bYvTKPhewIo",
-    authDomain: "crwn-apparel-db.firebaseapp.com",
-    databaseURL: "https://crwn-apparel-db.firebaseio.com",
-    projectId: "crwn-apparel-db",
-    storageBucket: "crwn-apparel-db.appspot.com",
-    messagingSenderId: "683732659394",
-    appId: "1:683732659394:web:4c0d906f9e5abfd8255f5b",
-    measurementId: "G-GBX272R8W0"
-};
+// const config = {
+//     apiKey: "AIzaSyArtvNfiP-5OoJnywHt0304bYvTKPhewIo",
+//     authDomain: "crwn-apparel-db.firebaseapp.com",
+//     databaseURL: "https://crwn-apparel-db.firebaseio.com",
+//     projectId: "crwn-apparel-db",
+//     storageBucket: "crwn-apparel-db.appspot.com",
+//     messagingSenderId: "683732659394",
+//     appId: "1:683732659394:web:4c0d906f9e5abfd8255f5b",
+//     measurementId: "G-GBX272R8W0"
+// };
 
 // const config = {
 //   apiKey: process.env.REACT_APP_API_KEY,
@@ -23,25 +23,25 @@ const config = {
 // };
 
 
-// const prodConfig = {
-//   apiKey: process.env.REACT_APP_PROD_API_KEY,
-//   authDomain: process.env.REACT_APP_PROD_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_PROD_DATABASE_URL,
-//   projectId: process.env.REACT_APP_PROD_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_PROD_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_PROD_MESSAGING_SENDER_ID,
-// };
+const prodConfig = {
+  apiKey: process.env.REACT_APP_PROD_API_KEY,
+  authDomain: process.env.REACT_APP_PROD_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_PROD_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROD_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_PROD_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_PROD_MESSAGING_SENDER_ID,
+};
 
-// const devConfig = {
-//   apiKey: process.env.REACT_APP_DEV_API_KEY,
-//   authDomain: process.env.REACT_APP_DEV_AUTH_DOMAIN,
-//   databaseURL: process.env.REACT_APP_DEV_DATABASE_URL,
-//   projectId: process.env.REACT_APP_DEV_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_DEV_MESSAGING_SENDER_ID,
-// };
+const devConfig = {
+  apiKey: process.env.REACT_APP_DEV_API_KEY,
+  authDomain: process.env.REACT_APP_DEV_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DEV_DATABASE_URL,
+  projectId: process.env.REACT_APP_DEV_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_DEV_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_DEV_MESSAGING_SENDER_ID,
+};
 
-// const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
+const config = process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,32 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component'
 
 import './shop.styles.scss';
-import SHOP_DATA from './shop.data.js';
+import { Route } from 'react-router-dom';
 
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
-class ShopPage extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render(){
-        const {collections} = this.state;
-        return (
-            <div className="shop-page">
-            { 
-                collections.map(({ id, ...otherCollectionProps }) => (
-                    <CollectionPreview key={id} {...otherCollectionProps} />
-                )) 
-            }
-            </div>
-        )
-    }
-
-}
+const ShopPage = ({ match }) => (
+    <div className="shop-page">
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+)
 
 export default ShopPage;
 
